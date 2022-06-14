e6af7709974e381c50559b6eefd0025bf01b054d
Om Sachin Gunjal <68736204+omgunjal@users.noreply.github.com>
2022-06-14 06:10:12 +0530

    Add files via upload

---
+++ Array/all_possible_subarrays.cpp
@@ -0,0 +1,27 @@
+// print all the possible subarrays of the given array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    for (int i=0; i<n; i++){
+        for (int j =i; j<n; j++){
+            for(int k=i; k<=j; k++){
+                cout<<arr[k]<<' ';
+            }
+            cout<<endl;
+        }
+    }
+
+    return 0;
+
+}
+
